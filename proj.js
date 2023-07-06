7bba19b328a9f2152b103118bebadfbd98d75142
Sagnik Datta <127123265+sagnik-datta-02@users.noreply.github.com>
2023-07-07 01:39:38 +0530

    Add files via upload

---
+++ proj.js
@@ -0,0 +1,10 @@
+import { Application } from '@splinetool/runtime';
+const canvas3 = document.getElementById('canvas5d');
+const app3 = new Application(canvas3);
+app3.load('https://prod.spline.design/y4krBhYepVXhS7q6/scene.splinecode');
+
+
+
+const canvas6 = document.getElementById('canvas6d');
+const app6 = new Application(canvas6);
+app6.load('https://prod.spline.design/3zAcCUTlGi3oXACQ/scene.splinecode');
